4b6fc197f8a4064b8b569abd8738231719618aeb
idror <144365018+idror67@users.noreply.github.com>
2024-11-19 23:58:07 +0200

    add all files

---
+++ fullstack_flask_mysql/provisioners/run-app.sh
@@ -0,0 +1,9 @@
+cd /flask-contacts
+python3 -m venv ~/.venv
+source ~/.venv/bin/activate
+
+# install requirements
+pip install -r /flask-contacts/requirements.txt
+
+# Start the application
+python3 app.py
---
+++ vagrant1/provisioners/install-nginx.sh
@@ -0,0 +1,4 @@
+echo "Installing Nginx"
+sudo apt update
+sudo apt install -y nginx
+echo "Nginx installed"
